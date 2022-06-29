6a8c21bc9d019ec7d292e298d7a096092e32cc97
Tural Guluev <tural-98@bk.ru>
2022-06-29 15:12:57 +0300

    Added an annotation @MethodSource

--- src/test/java/GeoServiceImplTest/GeoServiceImplTest.java
+++ src/test/java/GeoServiceImplTest/GeoServiceImplTest.java
@@ -3,11 +3,14 @@ package GeoServiceImplTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoServiceImpl;
 
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,47 +23,24 @@ class GeoServiceImplTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"127.0.01", "172.0.32.11", "96.44.183.149", "172.6334", "96.524", " "})
-    void byIp(String ip) {
-        Location location = geoService.byIp(ip);
-        if (ip.equals(GeoServiceImpl.LOCALHOST)) {
-            assertEquals(0, location.getBuiling());
-            assertNull(location.getCountry());
-            assertNull(location.getCity());
-            assertNull(location.getStreet());
-            return;
-        }
-        if (ip.equals(GeoServiceImpl.MOSCOW_IP)) {
-            assertEquals("Moscow", location.getCity());
-            assertEquals(Country.RUSSIA, location.getCountry());
-            assertEquals("Lenina", location.getStreet());
-            assertEquals(15, location.getBuiling());
-            return;
-        }
-        if (ip.equals(GeoServiceImpl.NEW_YORK_IP)) {
-            assertEquals("New York", location.getCity());
-            assertEquals(Country.USA, location.getCountry());
-            assertEquals(" 10th Avenue", location.getStreet());
-            assertEquals(32, location.getBuiling());
-            return;
-        }
-        if (ip.startsWith("172.")) {
-            assertEquals("Moscow", location.getCity());
-            assertEquals(Country.RUSSIA, location.getCountry());
-            assertNull(location.getStreet());
-            assertEquals(0, location.getBuiling());
-            return;
-        }
-        if (ip.startsWith("96.")) {
-            assertEquals("New York", location.getCity());
-            assertEquals(Country.USA, location.getCountry());
-            assertNull(location.getStreet());
-            assertEquals(0, location.getBuiling());
-            return;
-        }
-        assertNull(location);
+    @MethodSource("source")
+    void byIp(String ip, Location location) {
+        Location location1 = geoService.byIp(ip);
+        assertEquals(location, location1);
 
     }
+
+    static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of("127.0.0.1", new Location(null, null, null, 0)),
+                Arguments.of("172.0.32.11", new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
+                Arguments.of("96.44.183.149", new Location("New York", Country.USA, " 10th Avenue", 32)),
+                Arguments.of("172.243211", new Location("Moscow", Country.RUSSIA, null, 0)),
+                Arguments.of("96.24872491", new Location("New York", Country.USA, null, 0))
+
+        );
+    }
+
 
     @Test
     public void byCoordinates() {
